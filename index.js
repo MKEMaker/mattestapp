2ffe2d9e2435bfcb17b9066508a92a52d8b58d49
Andrew Klitzke <andrew@Andrews-MacBook-Air.local>
2018-12-11 20:13:14 -0600

    moving over old test app

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('views', path.join(__dirname, 'views'))
+
+app.set('view engine', 'ejs')
+
+app.get('/mat', (req, res) => {
+  res.send('hello!')
+})
+
+app.get('/', (req, res) => res.render('pages/index'))
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
